e1e8159162274ab5572eb8b963b1641df1b88d24
unknown <nataliespinoza16.02@gmail.com>
2024-10-15 03:58:50 -0400

    subiendo todo

--- node/database/firebase.js
+++ node/database/firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../Key.json');
+// const serviceAccount = require('../Key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
